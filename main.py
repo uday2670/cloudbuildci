3c2936a2a2130e017a2b90879c204df252501f0f
uday2670 <udaykommaraju@gmail.com>
2024-03-16 11:02:32 +0530

    second commit

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "dev udaykiran World")
+    name = os.environ.get("NAME", "dev udaykiran  cloudbuild we achived finally sucess this saturday ....hardwork will pay you buddy !!!!!!!!!!World")
     return f"Hello {name}!"
 
 
